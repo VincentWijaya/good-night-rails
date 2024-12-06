be32ff392d1f1b5c2bd599a491b99d9133f5a186
Vincent <vincent.wijaya@mekari.com>
2024-12-06 18:37:40 +0700

    rename create sleep tracking migration

--- db/migrate/20241206111548_create_sleep_trackings.rb
+++ db/migrate/20241206111548_create_sleep_trackings.rb
@@ -1,4 +1,4 @@
-class CreateTimeTrackings < ActiveRecord::Migration[7.0]
+class CreateSleepTrackings < ActiveRecord::Migration[7.0]
   def change
     create_table :sleep_trackings do |t|
       t.references :user, foreign_key: true, null: false
