16eeca2f2310dd6e227949421b2b344e7e41dfeb
Vincent <vincent.wijaya@mekari.com>
2024-12-06 18:38:37 +0700

    running migration

---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2024_12_06_112235) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "followings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "following_user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["following_user_id"], name: "index_followings_on_following_user_id"
+    t.index ["user_id", "following_user_id"], name: "user_following_index", unique: true
+    t.index ["user_id"], name: "index_followings_on_user_id"
+  end
+
+  create_table "sleep_trackings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "clock_in", null: false
+    t.datetime "clock_out", null: false
+    t.integer "sleep_duration", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sleep_trackings_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "api_key", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
+  end
+
+  add_foreign_key "followings", "users"
+  add_foreign_key "followings", "users", column: "following_user_id"
+  add_foreign_key "sleep_trackings", "users"
+end
