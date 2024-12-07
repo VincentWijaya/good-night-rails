607dc85c6f2b05acbb871a2842af2f43954ee17f
Vincent <vincent.wijaya@mekari.com>
2024-12-07 10:35:34 +0700

    create api to unfollow user

--- app/controllers/api/v1/followings_controller.rb
+++ app/controllers/api/v1/followings_controller.rb
@@ -10,6 +10,16 @@ module Api::V1
       end
     end
 
+    def unfollow
+      result = FollowingServices::Unfollow.call(current_user: current_user, params: follow_params)
+
+      if result.is_a?(FollowingServices::Unfollow::Error)
+        render json: { error: result.message }, status: :bad_request
+      else
+        render json: FollowingSerializer.new(result).serializable_hash
+      end
+    end
+
     private
 
     def follow_params
